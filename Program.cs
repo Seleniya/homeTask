3d76edea3b628880c0e1fae1ea17398faa2ecde5
Кристина Врещ <cikoriya@gmail.com>
2023-07-06 00:46:30 +0300

    добавила решение задачи

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Написать программу, которая из имеющегося массива
+ строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+*/
+
+
+
+
+Console.WriteLine("Введите какие-нибудь символы, в строку, 0 - 5 штук.");
+string a = Console.ReadLine()!;
+Console.WriteLine("Введите снова какие-нибудь символы, в строку, 0 - 5 штук.");
+string b = Console.ReadLine()!;
+Console.WriteLine("Опять введите какие-нибудь символы, в строку.");
+string c = Console.ReadLine()!;
+﻿string[] array1 = new string[] {a, b, c};
+string[] array2 = new string[array1.Length];
+
+
+void countLengthOfStrings(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    var s = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        s = string.Join(", ", array); 
+    }
+
+    Console.WriteLine(s);
+}
+countLengthOfStrings(array1, array2);
+Console.WriteLine("Я создам массив из строк, длиной до 3 символов. Искомый массив это: ");
+
+PrintArray(array2);
+
+
